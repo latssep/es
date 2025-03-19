12313a09196886e3a0be1e15138fa6500c350439
Elena Mashkova <lena90614mashkova@gmail.com>
2025-03-19 18:40:20 +0300

    + gpio 13

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ class Rcc {
     Rcc () {rcc_clock_setup_pll(&rcc_hse_16mhz_3v3[RCC_CLOCK_3V3_168MHZ]);};
 };
 
-Rcc clock_system;
+//Rcc clock_system;
 
 
 
@@ -19,9 +19,13 @@ int main() {
     gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
 
 
-    while (true) {
-        gpio_toggle(GPIOD, GPIO15);
-        for (volatile uint32_t i = 0; i < 2'000'000; ++i);
+    for (uint32_t ptime{0};true;++ptime) {
+        gpio_toggle(GPIOD, GPIO13);
+        if(ptime % 500'000<250'000)
+            gpio_set(GPIOD, GPIO15);  
+        else 
+            gpio_clear(GPIOD, GPIO15);
+        //for (volatile uint32_t i = 0; i < 2'000'000; ++i);
 
     }
 }
